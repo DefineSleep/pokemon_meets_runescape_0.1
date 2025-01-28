2a25b4ac1a4562b5026a8d3892f18d4eb817b085
DefineSleep <123536705+DefineSleep@users.noreply.github.com>
2025-01-28 12:24:20 -0500

    updates_combat

--- combat/combat.gd
+++ combat/combat.gd
@@ -28,24 +28,38 @@ func import_player_equiped_moves_to_menu():
 	
 	
 	
-func spawn_enemy_with_random_stats():
+func spawn_enemy_with_random_stats(_stats : Array):
 	var enemy_spawned = COMBAT_ENEMY.instantiate()
+	var health = _stats[0]
+	var defense = _stats[1]
+	var attack = _stats[2]
+	var speed = _stats[3]
 	
+
 	
-	
-	
-	
-	
+	printerr("enemy stats:"+"\nHP:"+str(health)+"\nDEF:"+str(defense)+"\nATK:"+str(attack)+"\nSPD:"+str(speed))
 	combat.add_child(enemy_spawned)
 	pass
 
 
+func randomize_enemy_stats(total_stats: int) -> Array:
+	var min_health = total_stats * 0.3
+	var max_health = total_stats * 0.5
+	var health = randi_range(min_health, max_health)
 
-func randomize_enemy_stats()->int:
-	return 0
-	
-	
+	var remaining_stats = total_stats - health
+	var min_defense = remaining_stats * 0.1
+	var max_defense = remaining_stats * 0.2
+	var defense = randi_range(min_defense, max_defense)
 
+	remaining_stats -= defense
+	var min_attack = remaining_stats * 0.2
+	var max_attack = remaining_stats * 0.3
+	var attack = randi_range(min_attack, max_attack)
+
+	remaining_stats -= attack
+	var speed = remaining_stats # Remaining stats go to speed
+	return [health , defense, attack, speed]
 
 
 func player_does_damage_to_enemy(_damage):
@@ -74,4 +88,6 @@ func write_and_instentiate_damage_message(_damage_amount, _entity_attacking_name
 
 # TEST BUTTON
 func _on_button_pressed() -> void:
-	write_and_instentiate_damage_message(1,"Test", "Test Move")
+	#write_and_instentiate_damage_message(1,"Test", "Test Move")
+	printerr("spawn enemy "+str(Global.all_enemies[0].enemy_name))
+	spawn_enemy_with_random_stats(randomize_enemy_stats(Global.all_enemies[0].total_stat_pool))
